39f315275eea5bc0fe9b3541dd5a6bce631e6a0a
CaioPaulinoCosta <caiopaulinocosta@hotmail.com>
2023-05-20 18:49:01 -0300

    Criado modelo de usuario

---
+++ dao/UserDAO.php
@@ -0,0 +1,57 @@
+<?php
+
+    require_once("models/User.php");
+
+    class UserDAO implements UserDAOInterface {
+
+        private $conn;
+        private $url;
+
+        public function __construct(PDO $conn, $url) {
+            $this->$conn = $conn;
+            $this->url = $url;
+        }
+
+        public function buildUser($data) {
+
+            $user = new User();
+
+            $user->id = $data["id"];
+            $user->id = $data["name"];
+            $user->id = $data["lastname"];
+            $user->id = $data["email"];
+            $user->id = $data["password"];
+            $user->id = $data["image"];
+            $user->id = $data["bio"];
+            $user->id = $data["token"];
+
+        }
+        public function create(User $user, $authUser = false) {
+
+        }
+        public function updtate(User $user) {
+
+        }
+        public function verifyToken($protected = false) {
+
+        }
+        public function setTokeToSession($token, $redirect = true) {
+
+        }
+        public function autheticateUser($email, $password) {
+
+        }
+        public function findByEmail($email) {
+
+        }
+        public function findById($id) {
+
+        }
+        public function findByToken($token) {
+
+        }
+        public function changePassword(User $user) {
+
+        }
+
+    }
