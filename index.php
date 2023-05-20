b1d9bb3dce669c64d1ae7d703927cc0ec1e1378b
CaioPaulinoCosta <caiopaulinocosta@hotmail.com>
2023-05-20 17:27:49 -0300

    Criado cabeçalho e rodapé

--- index.php
+++ index.php
@@ -1,38 +1,64 @@
-<?php
-
-require_once("globals.php");
-require_once("db.php");
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movie Star</title>
-    <link rel="short icon" href="img/moviestar.ico">
-
-  <!-- Bootstrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  
-  <!-- CSS do projeto -->
-  <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
-
+    <title>MovieStar</title>
+    <link rel="short icon" href="img/moviestar.ico" />
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous" />
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <!-- MovieStar CSS -->
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <h1 class="">Teste</h1>
+    <header>
+        <nav id="main-navbar" class="navbar navbar-expand-lg">
+            <a class="navbar-brand" href="#">
+                <img id="logo" src="img/logo.svg" alt="MovieStar">
+                <span id="moviestar-title">MovieStar</span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <form id="search-form" class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+                <button class="btn my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+              </form>
+            <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="<?= $BASE_URL ?>auth.php">Entrar / Cadastar</a>
+                </li>
+              </ul>
+              </div>
+          </nav>
+    </header>
+    <div id="main-container" class="container-fluid">
+      <h1>Corpo do site</h1>
+    </div>
+    <footer id="footer">
+      <div class="social-container">
+        <ul>
+          <li><a href=""></a><i class="fab fa-facebook-square"></i></li>
+          <li><a href=""></a><i class="fab fa-instagram"></i></li>
+          <li><a href=""></a><i class="fab fa-youtube"></i></li>
+        </ul>
+      </div>
+      <div id="footer-links-container"> 
+        <ul>
+          <li><a href="">Adicionar Filme</a></li>
+          <li><a href="">Adicionar Critica</a></li>
+          <li><a href="">Entrar / Cadastrar</a></li>
+        </ul>
+      </div>
+      <p>&copy;2023 Caio Paulino Costa</p>
+    </footer>
 </body>
-<!-- Bootstrap -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-
-<!-- Font Awesome -->
-<script src="https://kit.fontawesome.com/e4264bec92.js" crossorigin="anonymous"></script>
-
- <!-- Jquery -->
+<!-- BOOTSTRAP JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
-
- <!-- Bootstrap -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.js" integrity="sha512-KCgUnRzizZDFYoNEYmnqlo0PRE6rQkek9dE/oyIiCExStQ72O7GwIFfmPdkzk4OvZ/sbHKSLVeR4Gl3s7s679g==" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.js" integrity="sha512-KCgUnRzizZDFYoNEYmnqlo0PRE6rQkek9dE/oyIiCExStQ72O7GwIFfmPdkzk4OvZ/sbHKSLVeR4Gl3s7s679g==" crossorigin="anonymous"></script>
 </html>
