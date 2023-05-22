ca5419000adc09bdb2f6236a18f9523743422545
CaioPaulinoCosta <caiopaulinocosta@hotmail.com>
2023-05-21 23:09:10 -0300

    Correção de erro.

--- movie.php
+++ movie.php
@@ -71,7 +71,7 @@
       <p><?= $movie->description ?></p>
     </div>
     <div class="col-md-4">
-      <div class="movie-image-container" style="background-image: url('<?= $BASE_URL ?>img/movies/<?= $movie->image ?>')"></div>
+      <div class="movie-image-container w-50" style="background-image: url('<?= $BASE_URL ?>img/movies/<?= $movie->image ?>')"></div>
     </div>
     <div class="offset-md-1 col-md-10" id="reviews-container">
       <h3 id="reviews-title">Avaliações:</h3>
